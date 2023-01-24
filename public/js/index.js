69ee93b73b51e00a56d19311aaa09da2eeb332bb
Daniel Alonso <dan555lvl@gmail.com>
2023-01-24 10:00:32 -0300

    Site Beatriz BCV

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,7 @@ function scrollToPosition(to) {
 
 function scrollToIdOnClick(event) {
 	event.preventDefault();
-	const to = getScrollTopByHref(event.currentTarget) + 15;
+	const to = getScrollTopByHref(event.currentTarget)+ 3;
 	scrollToPosition(to);
 }
 
