1188a4eed831a1cf6cd09ed48a2bddca52809e01
Daniel Alonso <dan555lvl@gmail.com>
2023-01-23 22:02:36 -0300

    Site Beatriz BCV

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,8 @@
-const menuLinks = document.querySelectorAll('.principal-navbar > a[href^="#"]');
-
-function getDistanceFromTheTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
-}
-
-function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-  smoothScrollTo(0, distanceFromTheTop);
-}
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
+$("nav a").on("click", function (e) {
+  // 1 
+  e.preventDefault();
+  // 2 
+  const href = $(this).attr("href");
+  // 3 
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 200);
 });
-
-function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
-
-  duration = 2000
-
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1)
-      return (distance / 2) * time * time * time * time + from;
-    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-  };
-
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
-    }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
-}
